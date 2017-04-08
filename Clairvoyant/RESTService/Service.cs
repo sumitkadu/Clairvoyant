c7ca68073f162241873597b55f92982cff847d58
Sumit Kadu (sumitkadu) <sumit.kadu@live.com>
2017-04-08 16:09:08 +0530

    Added REST Service

---
+++ Clairvoyant/RESTService/Service.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace Clairvoyant
+{
+    [ServiceContract]
+    public class Service
+    {
+        [WebInvoke(UriTemplate = "/posterror", Method = "POST", RequestFormat = WebMessageFormat.Json,ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        public string PostMessage(string input)
+        {
+            return input;
+        }        
+    }
+}
